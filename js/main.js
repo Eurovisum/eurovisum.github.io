11c03be31b7bf101b2c5c4ddbe4d6e5f45542932
eurovisum <158383652+Eurovisum@users.noreply.github.com>
2024-02-01 01:24:42 +0100

    Initial

---
+++ js/main.js
@@ -0,0 +1,308 @@
+jQuery(function($) {
+
+  "use strict";
+
+/*------------------------------------------------------------------------------*/
+/* Menu
+/*------------------------------------------------------------------------------*/
+
+    var menu = {
+        initialize: function() {
+            this.Menuhover();
+        },
+
+        Menuhover : function(){
+            var getNav = $("nav.main-menu"),
+                getWindow = $(window).width(),
+                getHeight = $(window).height(),
+                getIn = getNav.find("ul.menu").data("in"),
+                getOut = getNav.find("ul.menu").data("out");
+            
+            if ( matchMedia( 'only screen and (max-width: 1200px)' ).matches ) {
+                                                     
+                // Enable click event
+                $("nav.main-menu ul.menu").each(function(){
+                    
+                    // Dropdown Fade Toggle
+                    $("a.mega-menu-link", this).on('click', function (e) {
+                        e.preventDefault();
+                        var t = $(this);
+                        t.toggleClass('active').next('ul').toggleClass('active');
+                    });   
+
+                    // Megamenu style
+                    $(".megamenu-fw", this).each(function(){
+                        $(".col-menu", this).each(function(){
+                            $(".title", this).off("click");
+                            $(".title", this).on("click", function(){
+                                $(this).closest(".col-menu").find(".content").stop().toggleClass('active');
+                                $(this).closest(".col-menu").toggleClass("active");
+                                return false;
+                                e.preventDefault();
+                                
+                            });
+
+                        });
+                    });  
+                    
+                }); 
+            }
+        },
+    };
+    
+    $('.btn-show-menu-mobile').on('click', function(e){
+        $(this).toggleClass('is-active'); 
+        $('.menu-mobile').toggleClass('show'); 
+        return false;
+        e.preventDefault();  
+    });
+
+   var $nav = $('.btn-show-menu-mobile');
+        $nav.click(function() {
+        $('.site-navigation').toggleClass('nav-open');
+    });
+
+    // Initialize
+    $(document).ready(function(){
+        menu.initialize();
+
+    });
+
+    var $menu = $('#menu'), $menulink = $('#menu-toggle-form'), $menuTrigger = $('.has-submenu > a');
+    $menulink.on('click',function (e) {
+
+        $menulink.toggleClass('active');
+        $menu.toggleClass('active');
+    });
+
+    $menuTrigger.on('click',function (e) {
+        e.preventDefault();
+        var t = $(this);
+        t.toggleClass('active').next('ul').toggleClass('active');
+    });
+
+
+/*------------------------------------------------------------------------------*/
+/* Animation on scroll: Number rotator
+/*------------------------------------------------------------------------------*/
+    
+    $("[data-appear-animation]").each(function() {
+    var self      = $(this);
+    var animation = self.data("appear-animation");
+    var delay     = (self.data("appear-animation-delay") ? self.data("appear-animation-delay") : 0);
+        
+        if( $(window).width() > 959 ) {
+            self.html('0');
+            self.waypoint(function(direction) {
+                if( !self.hasClass('completed') ){
+                    var from     = self.data('from');
+                    var to       = self.data('to');
+                    var interval = self.data('interval');
+                    self.numinate({
+                        format: '%counter%',
+                        from: from,
+                        to: to,
+                        runningInterval: 2000,
+                        stepUnit: interval,
+                        onComplete: function(elem) {
+                            self.addClass('completed');
+                        }
+                    });
+                }
+            }, { offset:'85%' });
+        } else {
+            if( animation == 'animateWidth' ) {
+                self.css('width', self.data("width"));
+            }
+        }
+    });
+
+ jQuery(".prt-circle-box").each(function () {
+
+        var circle_box = jQuery(this);
+        var fill_val = circle_box.data("fill");
+        var emptyFill_val = circle_box.data("emptyfill");
+        var thickness_val = circle_box.data("thickness");
+        var linecap_val = circle_box.data("linecap")
+        var fill_gradient = circle_box.data("gradient");
+        var startangle_val = (-Math.PI / 4) * 1.5;
+        if (fill_gradient != "") {
+            fill_gradient = fill_gradient.split("|");
+            fill_val = { gradient: [fill_gradient[0], fill_gradient[1]] };
+        }
+        if (typeof jQuery.fn.circleProgress == "function") {
+            var digit = circle_box.data("digit");
+            var before = circle_box.data("before");
+            var after = circle_box.data("after");
+            var digit = Number(digit);
+            var short_digit = digit / 100;
+            var size_val = circle_box.data("size");
+            jQuery(".prt-circle", circle_box)
+                .circleProgress({ value: 0, duration: 8000, size: size_val, startAngle: startangle_val, 
+                    thickness: thickness_val, linecap:linecap_val, emptyFill: emptyFill_val, fill: fill_val })
+                .on("circle-animation-progress", function (event, progress, stepValue) {
+                    
+                    circle_box.find(".prt-fid-number").html(before + Math.round(stepValue * 100) + after);
+                });
+        }
+        circle_box.waypoint(
+            function (direction) {
+
+                if (!circle_box.hasClass("completed")) {
+                    if (typeof jQuery.fn.circleProgress == "function") {
+                        jQuery(".prt-circle", circle_box).circleProgress({ value: short_digit });
+                    }
+                    circle_box.addClass("completed");
+                }
+            },
+            { offset: "90%" }
+        );
+    });
+
+ /* listimgbox */
+
+ jQuery(document).on('ready', function(e) {
+
+         jQuery('.prt_listimgbox_wrapper .prt_listimgbox_wrap').on('hover', function (e){
+            jQuery('.prt_listimgbox_wrapper .prt_listimgbox_wrap').removeClass("active");
+            jQuery(this).addClass("active");
+         });
+ });
+
+ /* footer_customheading */
+ jQuery(window).on('load', function (e){
+    var $li = jQuery('.footer_customheading span');
+    $li.hide().first().show().addClass('active');
+
+    function footerloop() {
+        jQuery('.footer_customheading .active').each(function(index){
+            var $this = jQuery(this);
+            var $next = $this.next().length > 0 ? $this.next() : $li.first();
+
+            $this.hide().removeClass('active');
+            $next.show().addClass('active');
+
+            if( $next.index() == 0) {
+               // clearInterval(myTimer);
+                setTimeout(function(){
+                }, 3000);
+            }
+        });
+    }
+
+    setInterval(function(){footerloop()},2000);//timer running every 2 seconds
+
+
+});
+
+/*------------------------------------------------------------------------------*/
+/* Tab
+/*------------------------------------------------------------------------------*/ 
+    $('.prt-tabs').each(function() {
+        $(this).children('.content-tab').children().hide();
+        $(this).children('.content-tab').children().first().show();
+        $(this).find('.tabs').children('li').on('click', function(e) {  
+        var liActive = $(this).index(),
+        contentActive = $(this).siblings().removeClass('active').parents('.prt-tabs').children('.content-tab').children().eq(liActive);
+        contentActive.addClass('active').fadeIn('slow');
+        contentActive.siblings().removeClass('active');
+        $(this).addClass('active').parents('.prt-tabs').children('.content-tab').children().eq(liActive).siblings().hide();
+        e.preventDefault();
+        });
+    });
+
+    $(document).on('ready', function(e) {
+        $('.prt-tabs.slider-tab > .tabs').children('li').on('click', function(e) {  
+            var tab = $(this).closest('.prt-tabs > .tabs > .tab'), 
+            index = $(this).closest('.prt-tabs > .tabs > li').index();
+            $(this).parents('.prt-tabs').children(' .tabs').children('li.active ').removeClass('active'); 
+            $(this).addClass('active'); 
+            $(this).addClass('active').parents('.prt-tabs').children('.content-tab').find('.content-inner').not('.content-inner:eq(' + index + ')').slideUp();
+            $(this).addClass('active').parents('.prt-tabs').children('.content-tab').find('.content-inner:eq(' + index + ')').slideDown();
+            e.preventDefault();
+        });
+    });
+
+
+/*------------------------------------------------------------------------------*/
+/* Accordion
+/*------------------------------------------------------------------------------*/
+
+    var allPanels = $('.accordion > .toggle').children('.toggle-content').hide();
+
+    $('.toggle-title').on('click',function(e) {
+
+        e.preventDefault();
+        var $this = $(this);
+            $this.parent().parent().find('.toggle .toggle-title a').removeClass('active');
+
+        if ($this.next().hasClass('show')) {
+
+            $this.next().removeClass('show');   
+            $this.next().slideUp('easeInExpo');
+
+        } else {
+            $this.parent().parent().find('.toggle .toggle-content').removeClass('show');
+            $this.parent().parent().find('.toggle .toggle-content').slideUp('easeInExpo');
+            $this.next().toggleClass('show');
+            $this.next().removeClass('show');
+            $this.next().slideToggle('easeInExpo');
+           $this.next().parent().children().children().addClass('active');
+
+        }
+
+    });
+
+/*------------------------------------------------------------------------------*/
+/* Custom select
+/*------------------------------------------------------------------------------*/
+
+    $('#selectbox').each(function(){
+    var $this = $(this), numberOfOptions = $(this).children('option').length;
+  
+    $this.addClass('select-hidden'); 
+    $this.wrap('<div class="select"></div>');
+    $this.after('<div class="select-styled"></div>');
+
+    var $styledSelect = $this.next('div.select-styled');
+    $styledSelect.text($this.children('option').eq(0).text());
+  
+    var $list = $('<ul />', {
+        'class': 'select-options'
+    }).insertAfter($styledSelect);
+  
+    for (var i = 0; i < numberOfOptions; i++) {
+        $('<li />', {
+            text: $this.children('option').eq(i).text(),
+            rel: $this.children('option').eq(i).val()
+        }).appendTo($list);
+    }
+  
+    var $listItems = $list.children('li');
+  
+    $styledSelect.on("click",function(e) {
+        e.stopPropagation();
+        $('div.select-styled.active').not(this).each(function(){
+            $(this).removeClass('active').next('ul.select-options').hide();
+        });
+        $(this).toggleClass('active').next('ul.select-options').toggle();
+    });
+  
+    $listItems.on("click",function(e) {
+        e.stopPropagation();
+        $styledSelect.text($(this).text()).removeClass('active');
+        $this.val($(this).attr('rel'));
+        $list.hide();
+        //console.log($this.val());
+    });
+  
+    $(document).on("click",function(e) {
+        $styledSelect.removeClass('active');
+        $list.hide();
+    });
+
+    });
+
+
+
+ });
